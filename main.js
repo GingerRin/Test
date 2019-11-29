6e20d8e85793891f8b3ab350ce8affe3c6b71cd8
GingerRin <youarenottakingthisseriously@gmail.com>
2019-11-29 18:30:27 +0800

    bunch of stuff added
    rmb to finish rand script

---
+++ main.js
@@ -0,0 +1,9 @@
+function randomArrow(){
+	var result = ""
+	var arrow = ["up","down","left","right"]
+	for (var i = arrow.length - 1; i >= 0; i--) {
+		
+	}
+	document.getElementById("arrows").className = "left";
+	/*document.getElementById("arrows").className = "";*/
+}
