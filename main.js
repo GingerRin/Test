06023c968808797fba27c0b5c30174e2cfd21ab1
GingerRin <youarenottakingthisseriously@gmail.com>
2019-11-30 21:04:09 +0800

    Many things
    yep

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
-function randomArrow(){
-	var result = ""
-	var arrow = ["up","down","left","right"]
-	for (var i = arrow.length - 1; i >= 0; i--) {
-		
-	}
-	document.getElementById("arrows").className = "left";
-	/*document.getElementById("arrows").className = "";*/
+var seq = 0;
+var audio = new Audio()
+var playlist = new Array(
+	'resources/audio/1 Are.mp3',
+	'resources/audio/2 You.mp3',
+	'resources/audio/3 Rea.mp3',
+	'resources/audio/4 dy.mp3',
+	'resources/audio/5 Im.mp3',
+	'resources/audio/6 a.mp3',
+	'resources/audio/7 La.mp3',
+	'resources/audio/8 dy.mp3',
+	'resources/audio/9 Ha.mp3',
+	'resources/audio/10 ji.mp3',
+	'resources/audio/11 me.mp3',
+	'resources/audio/12 yo.mp3',
+	'resources/audio/13 u.mp3',
+	'resources/audio/14 Ya.mp3',
+	'resources/audio/15 re.mp3',
+	'resources/audio/16 ba.mp3',
+	'resources/audio/17 De.mp3',
+	'resources/audio/18 ki.mp3',
+	'resources/audio/19 ru.mp3',
+	'resources/audio/20 Kit.mp3',
+	'resources/audio/21 to.mp3',
+	'resources/audio/22 Zet.mp3',
+	'resources/audio/23 ta.mp3',
+	'resources/audio/24 i.mp3',
+	)
+
+document.getElementById("screen").addEventListener("click", playSeq)
+
+function playSeq(){
+	if(seq<=23){
+	audio.src = playlist[seq]
+	audio.play();
+	seq ++;}
 }
